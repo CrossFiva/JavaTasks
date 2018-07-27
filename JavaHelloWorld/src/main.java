df311bd5a5e2f86b69000b680e7b6dddfdca8b82
fiva1987 <fiva1987@gmail.com>
2018-07-27 14:32:36 +1000

    new function

---
+++ JavaHelloWorld/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+    public static boolean function1 (int a) {
+    	return true;
+    }
+
+}
